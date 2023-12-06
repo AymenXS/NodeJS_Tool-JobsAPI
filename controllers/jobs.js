2a890195f42ec77bc5a2144b84d897c1f538a923
Aymen <aymenghaloua@gmail.com>
2023-12-07 00:16:50 +0100

    UPDATING

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,16 +3,20 @@ const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, NotFoundError } = require('../errors');
 
 const getAllJobs = async (req, res) => {
-  res.send('get all jobs');
+  const jobs = await Job.find({ createdBy: req.user.userID }).sort('createdAt');
+  res.status(StatusCodes.OK).send({ jobs, count: jobs.length });
 };
+
 const getJob = async (req, res) => {
   res.send('get job');
 };
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userID;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).send(job);
 };
+
 const updateJob = async (req, res) => {
   res.send('update job');
 };
