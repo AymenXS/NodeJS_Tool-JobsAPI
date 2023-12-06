262cbd7f676c0450ed70dbff2fe7e47485ca165a
Aymen <aymenghaloua@gmail.com>
2023-12-06 06:30:19 +0100

    UPDATING: A lot..

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -12,7 +12,7 @@ const auth = async (req, res, next) => {
   const token = authHeader.split(' ')[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { userId: payload.userId, name: payload.name };
+    req.user = { userID: payload.userId, name: payload.name };
     next();
   } catch (error) {
     throw new UnauthenticatedError('Authentication Invalid');
